2a801addf417ad55dcd4dd41b5c0df3b5005f806
dixitVora <dixitvora@gmail.com>
2024-01-01 17:49:10 +0530

    main: added the initial setup with vite and react and pwa

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import CardComponent from './components/CardComponent'
+
+function App() {
+
+  return (
+    <>
+      <CardComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
+
+let faviconURL = '/vit.svg'
+
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    injectRegister: 'auto',
+    includeAssets: [faviconURL],
+    registerType: 'prompt',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+      navigateFallback: 'index.html',
+    },
+    manifest: {
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: faviconURL,
+          sizes: '512x512',
+          type: 'image/svg+xml',
+          purpose: 'any maskable'
+        },
+        {
+          src: faviconURL,
+          sizes: '512x512',
+          type: 'image/png',
+        }
+      ]
+    },
+  })],
+});
